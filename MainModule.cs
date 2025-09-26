d7a718bc6655568b41d253d07989363c6f1b5b09
Raphael Wu <raphael0422@gmail.com>
2025-09-26 18:07:51 +0800

    Add files via upload

---
+++ MainModule.cs
@@ -0,0 +1,13 @@
+﻿using Autofac;
+using Calin.TaskPulse.Views;
+
+namespace Calin.TaskPulse
+{
+    public class MainModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<MainMenuControl>().AsSelf().PropertiesAutowired();
+        }
+    }
+}
